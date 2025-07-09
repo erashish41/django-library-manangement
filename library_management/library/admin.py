df9b028c06ab7e2d063b4df3c6b580b06759a857
Ashish Bhardwaj <erashish41@gmail.com>
2025-07-09 19:30:36 +0530

    models updated

--- library_management/library/admin.py
+++ library_management/library/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from library.models import (IssuedBook, Member, Book, Author, Publisher,
+                            Category, LibraryBranch, City)
 
 # Register your models here.
+
+admin.site.register(IssuedBook)
+admin.site.register(Member)
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Publisher)
+admin.site.register(Category)
+admin.site.register(LibraryBranch)
+admin.site.register(City)
--- library_management/library/models.py
+++ library_management/library/models.py
@@ -1,4 +1,3 @@
-
 from django.contrib.auth.models import User
 from django.db import models
 from utils.models import BaseMixin
