cf652a0d1bf4fba3e9e0c4f5c9db93898e05ffe7
Ashish Bhardwaj <erashish41@gmail.com>
2025-07-10 23:51:43 +0530

    message added

--- library_management/library/views.py
+++ library_management/library/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView
-
+from library.models import IssuedBook, Member, Book, Author, Publisher, Category, LibraryBranch, City
 # Create your views here.
+
+
+class BookList(ListView, CreateView):
+    model = Book
+    
--- library_management/library_management/settings.py
+++ library_management/library_management/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -68,6 +70,14 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'library_management.wsgi.application'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: "alert-secondary",
+    messages.INFO: "alert-info",
+    messages.SUCCESS: "alert-success",
+    messages.WARNING: "alert-warning",
+    messages.ERROR: "alert-danger",
+}
 
 
 # Database
